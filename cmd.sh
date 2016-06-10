a68cc0cf58a06796479f3d52fa78f43be53a4edf
Haani Niyaz <haani.niyaz@gmail.com>
2016-06-10 18:11:35 +1000

    Switched backed to flask debugging mode

--- cmd.sh
+++ cmd.sh
@@ -4,8 +4,8 @@ set -e
 if [ "$ENV" = 'DEV' ];
 then
 	echo 'Running Dev server'
-	exec uwsgi --http 0.0.0.0:9090 --catch-exceptions --wsgi-file /app/identidock.py \
-             --callable app --stats 0.0.0.0:9191
+ 	exec python "/app/identidock.py"
+
 else
 	echo 'Running Prod server'
 	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
