04e3dbb695610c64409b47ab07c341eff35cd0c0
Haani Niyaz <haani.niyaz@gmail.com>
2016-06-10 16:03:59 +1000

    Added debug mode wsgi in dev

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
-exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+
+if [ "$ENV" = 'DEV' ];
+then
+	echo 'Running Dev server'
+	exec uwsgi --http 0.0.0.0:9090 --catch-exceptions --wsgi-file /app/identidock.py \
              --callable app --stats 0.0.0.0:9191
+else
+	echo 'Running Prod server'
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+             --callable app --stats 0.0.0.0:9191
+fi
