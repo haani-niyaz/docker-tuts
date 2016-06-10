037ad08336149930b77bfd4f6ec3056698e44aec
Haani Niyaz <haani.niyaz@gmail.com>
2016-06-10 20:22:28 +1000

    Added decorator to serve /monster/<name> route

--- app/identidock.py
+++ app/identidock.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import requests
 app = Flask(__name__)
 
 @app.route('/')
-def hell_world():
+def main_page():
 	# return 'Hello Docker!\n'
-	name = "Chuck Norris"
+	name = "Billy"
 	return render_template('index.html', name=name)
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+	r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+	image = r.content
+	return Response(image,mimetype='image/png')
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0')
