27de65af50f34fa1acb85e71382ab731d1368e37
Haani Niyaz <haani.niyaz@gmail.com>
2016-06-12 18:18:25 +1000

    Added env for unit tests

--- cmd.sh
+++ cmd.sh
@@ -5,7 +5,9 @@ if [ "$ENV" = 'DEV' ];
 then
 	echo 'Running Dev server'
  	exec python "/app/identidock.py"
-
+elif [ "$ENV" = 'UNIT' ]; then
+	echo 'Running Unit Tests'
+	exec python "/app/test.py"
 else
 	echo 'Running Prod server'
 	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
